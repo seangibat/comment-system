a0e80b66dddf7e284ba321b59b96049c22c0dcf5
Sean Gibat <sgibat@gmail.com>
2015-06-17 02:39:48 -0700

    update routes

--- comment-system/routes/api.js
+++ comment-system/routes/api.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var threadController = require('../controllers/threadController.js');
 var postController = require('../controllers/postController.js');
+var voteController = require('../controllers/voteController.js');
 
 /* GET home page. */
 router
-  .get('/threads', threadController.getThreads)
   .get('/thread/:threadId', threadController.getThread)
+  .get('/thread', threadController.getFirstThread)
   .post('/thread', threadController.createThread)
-  .get('/post/:postId', postController.getPost)
+  .post('/vote', voteController.vote)
   .post('/post', postController.createPost);
 
 module.exports = router;
